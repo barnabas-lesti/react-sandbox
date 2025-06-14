72c313169ce414c6a3bb43f92728bea2d7a16301
Barnabas Lesti <barnabas.lesti@gmail.com>
2025-06-14 11:08:20 +0200

    docs(*): add environment support with typechecking

--- global.d.ts
+++ global.d.ts
@@ -1,1 +1,15 @@
 /// <reference types="vite/client" />
+
+// Add custom environment variables here for typecheck and intellisense.
+interface ImportMetaEnv {
+  /**
+   * Port to run the application on.
+   * @example
+   * PORT=4000
+   */
+  readonly PORT?: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,25 @@
 import reactPlugin from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import tsconfigPathsPlugin from "vite-tsconfig-paths";
 
-const port = 3000;
+/**
+ * Vite build configuration definition.
+ *
+ * https://vite.dev/config
+ */
+export default defineConfig((config) => {
+  const env = loadEnv(config.mode, "", "") as ImportMetaEnv;
+  const port = Number(env.PORT ?? 3000);
 
-// https://vite.dev/config/
-export default defineConfig({
-  root: "./src",
-  envDir: "..",
-  server: { port },
-  preview: { port },
-  build: {
-    outDir: "../dist",
-    emptyOutDir: true,
-  },
-  plugins: [tsconfigPathsPlugin(), reactPlugin()],
+  return {
+    root: "./src",
+    envDir: "..",
+    server: { port },
+    preview: { port },
+    build: {
+      outDir: "../dist",
+      emptyOutDir: true,
+    },
+    plugins: [tsconfigPathsPlugin(), reactPlugin()],
+  };
 });
