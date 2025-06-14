45fd93cc6f4b95d263a2d71b256882f21cfbe67a
Barnabas Lesti <barnabas.lesti@gmail.com>
2025-06-14 09:35:41 +0200

    feature(*): add prettier code formatter

---
+++ prettier.config.js
@@ -0,0 +1,19 @@
+/**
+ * Workspace prettier configuration.
+ * @type {import("prettier").Options}
+ */
+export const PRETTIER_CONFIG = {
+  printWidth: 120,
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  singleQuote: false,
+  quoteProps: "consistent",
+  jsxSingleQuote: false,
+  trailingComma: "all",
+  bracketSpacing: true,
+  bracketSameLine: false,
+  arrowParens: "always",
+  endOfLine: "auto",
+  singleAttributePerLine: true,
+};
